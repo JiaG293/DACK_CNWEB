19421bbd7391df933fde1d9e904d08c5729c392a
JiaG <giau32002@gmail.com>
2022-04-24 08:46:09 +0700

    24/04/2022 update 19

--- Layout web tin tuc/js/boxcomment.js
+++ Layout web tin tuc/js/boxcomment.js
@@ -11,7 +11,9 @@ $( document ).ready(function() {
         let hour = d.getHours();
         let minutes = d.getMinutes();
         let dateNow = date +"/"+ month +"/"+ year +"  " + hour +":" + minutes;
-        let row = "<li class='table-bordered bg-info'><div class='lh'><strong class='text-info'>"+userbox+"</strong><sub><code>"+dateNow+"</code></sub></div><div class='text-success'>"+cmtbox+"</div></li>";
+        /* Form binh luan cu */
+        //let row = "<li class='table-bordered bg-info'><div class='lh'><strong class='text-info'>"+userbox+"</strong><sub><code>"+dateNow+"</code></sub></div><div class='text-success'>"+cmtbox+"</div></li>";
+        let row = "<li class='table-bordered bg-info'><div class='media'> <div class='media-left'> <img src='../data/user-64.png' class='media-object' style='width:60px'> </div> <div class='media-body'> <h4 class='media-heading text-info'><strong>"+userbox+"</strong><sub><code>"+dateNow+"</code></sub></h4><dd class='text-success' style='font-size: small;'>"+cmtbox+"</dd> </div> </div></li>" ;
         $("#area-cmt").append(row);
         
     }); 
