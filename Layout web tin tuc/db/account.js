d8f3089a3868ccde6d38a959d2eee9bc4a0afde7
JiaG <giau32002@gmail.com>
2022-04-25 15:44:50 +0700

    25/04/2022 update 32

---
+++ Layout web tin tuc/db/account.js
@@ -0,0 +1,17 @@
+/* object dang ki push vao account array object */
+var a = {user:"", password:""};
+
+/* array object chua thong tin dang nhap user */
+var account = 
+[
+
+    {user:"user", password:"user"}, 
+
+    {user:"admin", password:"admin"}, 
+
+    {user:"user1", password:"user1"}, 
+
+    {user:"flymetothemoon", password:"flymetothemoon"} 
+
+]
+    
--- Layout web tin tuc/js/login.js
+++ Layout web tin tuc/js/login.js
@@ -5,30 +5,40 @@ $(document).ready(function(){
     });
     
     /* check nhap thong tin */
-    let us = $("#username").val(); 
-    let pwd = $("#password").val(); 
+    
+     
+    
     function checkUserName()
     {
-        let cUN = /^[a-zA-Z0-9]$/i;
+        let us = $("#username").val(); 
+        let cUN = /^[a-z0-9]{6,45}$/;
         if(us.trim()=="")
         {
             $("#errusername").html("Không để trống!!");
+            return false;
         }
-        if(!us.test(cUN)){
+        if(!cUN.test(us)){
             $("#errusername").html("Sai định dạng!!");
+            return false;
         }
+        
         $("#errusername").html("(*)");
+        return true
     }
     $("#username").keyup(checkUserName);
+    
     function checkPassword()
     {
-        let cPWD = /^[a-zA-Z0-9]$/i;
+        let pwd = $("#password").val();
+        let cPWD = /^[A-Za-z0-9(!#$%&'@*+-/=?^_`{|}~)]{8,100}$/;
         if(pwd.trim()=="")
         {
             $("#errpassword").html("Không để trống!!");
+            return false;
         }
-        if(!pwd.test(cPWD)){
+        if(!cPWD.test(pwd)){
             $("#errpassword").html("Sai định dạng!!");
+            return false;
         }
         $("#errpassword").html("(*)");
     }
@@ -37,49 +47,53 @@ $(document).ready(function(){
 
 
     /* xac thuc dang nhap */
-    //var account = {name:[], password:[]};
-    var account = {admin:"admin", user:"user"};
     function register()
     {
-        usr = $("#username").val(); 
-        pwr = $("#password]").val(); 
-        account["name"].push(usr);
-        account["password"].push(pwr);
+        var usr = $("#username").val(); 
+        var pwr = $("#password]").val(); 
+        a.user = usr;
+        a.password = pwr;
+        account.push(a);
+        $("#modal-login").modal("hide");
+        $("#text-notification").html("Đăng kí thành công!!");
+        $("#modal-notification").modal();
     }
-    $("#register").click(register);
-    
+    $("#regis").click(register);
+    let i = 0
     function login()
     {
         let usl = $("#username").val(); 
         let pwl = $("#password").val(); 
-        let u = Object.keys(account);
-        let aus = Object.values(account);
-        let us = aus;
-        alert(aus);
-        if(usl == u.split(","))
+        for(i = 0; i < 7 ; i++)
         {
-            if(pwl == aus.split(","))
+            if(usl == account[i].user )
+            {   
+                
+                if(pwl == account[i].password)
                 {
-                    
                     $("#modal-login").modal("hide");
                     $("#text-notification").html("Đăng nhập thành công!!");
                     $("#modal-notification").modal();
+                    break;
                 }
-            else
+                else
                 {
                     $("#text-notification").html("Sai mật khẩu!!");
                     $("#modal-notification").modal();
+                    break;
                 }
-        }
-        else
-        {
-            $("#text-notification").html("Sai tài khoản!!");
-            $("#modal-notification").modal();
+                
+            }
+            else
+            {
+                $("#text-notification").html("Sai tài khoản!!");
+                $("#modal-notification").modal();
+            }
         }
     }
+    
     $("#login").click(login);
     $("#modal-login").modal('hide');  
-    
 });
 
     
