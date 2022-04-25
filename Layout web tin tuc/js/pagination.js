b220f4303be9da39343ddaf9c853a440a7134e0b
JiaG <giau32002@gmail.com>
2022-04-25 07:38:02 +0700

    25/04/2022 update 26

--- Layout web tin tuc/js/pagination.js
+++ Layout web tin tuc/js/pagination.js
@@ -1,3 +1,24 @@
-$(document).ready(function(){
-    
+
+$(document).ready(function({
+    let arrow = $("#next-page, #prev-page").not("li a")
+
+    $("#bnext").click(function () {
+        let MaTour = $("#tfMaTour").val();
+        let HanhTrinh = $("#tfHanhTrinh").val();
+        let NgayKhoiHanh = $("#tfNgayKhoiHanh").val();
+        let ThoiGian = $("#tfThoiGian").val();
+        let GiaTour = $("#tfGiaTour").val();
+        let AnhDaiDien = $("#tfAnhDaiDien").val();
+        if (!checkMaTour()||!checkNgayKhoiHanh||!checkGiaTour()||!checkHanhTrinh()) 
+        {
+            alert("Mời bạn nhập đúng và đầy đủ thông tin");
+            //$("#errLuu").html("Mời bạn nhập đúng và đầy đủ thông tin");
+            return false;
+        }
+        let row = "<tr><td>"+(++i)+"</td><td>"+MaTour+"</td><td>"+HanhTrinh+"</td><td>"+NgayKhoiHanh+"</td><td>"+ThoiGian+"</td><td>"+GiaTour+"</td><td>"+AnhDaiDien+"</td></tr>"
+        $("#tbm").append(row);
+        $("#openModal").modal("hide");
+        return true;
+  });
+ 
 });
