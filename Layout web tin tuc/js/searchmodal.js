717d4ad28f7dbf58a59cb0827df3299f299632b9
JiaG <giau32002@gmail.com>
2022-04-28 13:31:47 +0700

    28/04/2022 update 56

--- Layout web tin tuc/js/searchmodal.js
+++ Layout web tin tuc/js/searchmodal.js
@@ -5,25 +5,18 @@ $(document).ready(function(){
     });
     $.ajaxSetup({ cache: false });
     $("#input-searchbox").keyup(function(){
-    $("#result-searchbox").html("");
-    //$('#state-searchbox').val("");
-    var searchField = $('#input-searchbox').val();
-    var expr = new RegExp(searchField, "i");
-    $.getJSON("../db/datasearchbox.json", function(data) {
-        $.each(data, function(key, value){
-        if (value.name.search(expr) != -1 || value.location.search(expr) != -1)
-        {
-            //$('#result').append('<li class="list-group-item link-class"><img src="'+value.image+'" height="40" width="40" class="img-thumbnail" /> '+value.name+' | <span class="text-muted">'+value.category+'</span><a target="_blank" href="'+value.link + '"><img class"navbar-link" src="../data/chevron-right-regular-24.png" alt="link"></a></li>');
-            $("#result-searchbox").append('<li class="list-group-item link-class"> '+value.name+' | <span class="text-muted">'+value.category+'</span><a class="changeColor" target="_blank" href="'+value.link + '"><img src="../data/arrow-24.png" alt="link"></a></li>');
-        }
-        });   
-     });
-    });
-    
-    $("#result-searchbox").on("click", "li", function() {
-     var click_text = $(this).text().split("|");
-    $("#input-searchbox").val($.trim(click_text[0]));
-    $("#result-searchbox").html("");
+        $("#result-searchbox").html("");
+        var searchField = $('#input-searchbox').val();
+        var expr = new RegExp(searchField, "i"); // dung regex search i khong phan biet chu cai
+        $.getJSON("../db/datasearchbox.json", function(datasearchbox){
+            $.each(datasearchbox, function(index, val){
+                if (val.name.search(expr) != -1 || val.location.search(expr) != -1)
+                {
+                    //$('#result-searchbox').append('<li class="list-group-item link-class"><img src="'+val.image+'" height="40" width="40" class="img-thumbnail" /> '+value.name+' | <span class="text-muted">'+val.category+'</span><a target="_blank" href="'+val.link + '"><img class"navbar-link" src="../data/chevron-right-regular-24.png" alt="link"></a></li>');
+                    $("#result-searchbox").append('<li class="list-group-item link-class"> '+index+" | "+val.name+' | <span class="text-muted">'+val.category+'</span><a class="changeColor" target="_blank" href="'+val.link + '"><img src="../data/arrow-24.png" alt="link"></a></li>');
+                }
+            });   
+        });
     });
     $("#button-searchbox").modal("hide");
 });
