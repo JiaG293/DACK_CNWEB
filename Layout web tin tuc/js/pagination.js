77c077e0a4f728a72b1064c96130cf8ca76da191
JiaG <giau32002@gmail.com>
2022-04-28 19:26:30 +0700

    28/04/2022 update 60

--- Layout web tin tuc/js/pagination.js
+++ Layout web tin tuc/js/pagination.js
@@ -1,9 +1,7 @@
 
-$(document).click(function(){
-
+$(document).ready(function(){
     //let alt = $(".img-readmore").attr("alt");
-    
-    function loadingList(datapagination)
+    $("#morenew").on("click", function()
     {
         datapagination.forEach(lc =>{
             //let idcheck = lc.id;
@@ -13,6 +11,5 @@ $(document).click(function(){
                 let arr = "<article class='box-readmore'><img class='img-readmore img-rounded' src='"+lc.img+"'alt='kiem song'><a href='"+lc.link+"' target='_blank' class='link-readmore'><div class='tittle-readmore'>"+lc.tittle+"</div> <div class='content-readmore'>"+lc.content+"</div></a></article>";
                 $("#readmore").append(arr); 
         });
-    }
-    $("#morenew").click(loadingList(datapagination));
+    });
 });
