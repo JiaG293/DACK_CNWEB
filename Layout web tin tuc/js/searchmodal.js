e531479c1a7ecd7c3ed5fcba961631351078abee
JiaG <giau32002@gmail.com>
2022-04-29 09:47:18 +0700

    29/04/2022 update 67

--- Layout web tin tuc/js/searchmodal.js
+++ Layout web tin tuc/js/searchmodal.js
@@ -11,8 +11,8 @@ $(document).ready(function(){
             $.each(datasearchbox, function(index, value){
                 if (value.name.search(expr) != -1)
                 {
-                    //$('#result-searchbox').append('<li class="list-group-item link-class"><img src="'+val.image+'" height="40" width="40" class="img-thumbnail" /> '+value.name+' | <span class="text-muted">'+val.category+'</span><a target="_blank" href="'+val.link + '"><img class"navbar-link" src="../data/chevron-right-regular-24.png" alt="link"></a></li>');
-                    $("#result-searchbox").append('<li class="list-group-item link-class"> '+index+" | "+value.name+' | <span class="text-muted">'+value.category+'</span><a class="changeColor" target="_blank" href="'+value.link + '"><img src="../data/arrow-24.png" alt="link"></a></li>');
+                    //$('#result-searchbox').append('<li class="list-group-item link-class"><img src="'+value.image+'" height="40" width="40" class="img-thumbnail" /> '+value.name+' | <span class="text-muted">'+val.category+'</span><a target="_blank" href="'+val.link + '"><img class"navbar-link" src="../data/chevron-right-regular-24.png" alt="link"></a></li>');
+                   $("#result-searchbox").append('<li class="list-group-item link-class"> '+index+" | "+value.name+' | <span class="text-muted">'+value.category+'</span><a class="changeColor" target="_blank" href="'+value.link + '"><img src="../data/arrow-24.png" alt="link"></a></li>');
                 }
             });   
         });
